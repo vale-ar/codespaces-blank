0ae883274812d870dc19cf65693d8baa9b53d44b
vale-ar <valentina.arancibia@genstudents.org>
2024-12-03 23:20:40 +0000

    cambios

---
+++ diccionario.py
@@ -0,0 +1,10 @@
+student_data = {
+    'nombre': 'José',
+    'apellido': 'Martínez',
+    'edad': 30,
+    'dirección': 'calle',
+    'graduado': True,
+    'materias_cursadas': ['python', 'javascript', 'sql']
+}
+
+print(student_data)
